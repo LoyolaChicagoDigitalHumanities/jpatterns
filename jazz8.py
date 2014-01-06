263983832b89437cdd082d009bb3af865a7d0984
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-01-05 22:35:07 -0600

    merging jazz8 and jazz8fuse examples

--- jazz8.py
+++ jazz8.py
@@ -10,17 +10,19 @@ import common, sideman
 # I didn't figure out how to combine two Measure instances into a single one via Abjad
 #
 
-def get_pattern8(jazz_scale, measure):
+def get_pattern8(jazz_scale):
     pitches = jazz_scale.get_named_pitches([1, 3, 5, 8])
     durations = [sideman.eighth] * 4
     notes = scoretools.make_notes(pitches, durations)
 
+    measure = Measure((2, 4))
     for note in notes: 
         measure.append(note)
     return measure
 
-def get_pattern8_chord_measure(jazz_scale, measure):
+def get_pattern8_chord_measure(jazz_scale):
     pitches = jazz_scale.get_chord_as_named([1 ,3, 5])
+    measure = Measure((2, 4))
     chord = Chord(pitches, (2, 4))
     measure.append(chord)
     return measure
@@ -29,20 +31,24 @@ def get_score():
     treble_pattern = Staff()
     chords = Staff(context_name='ChordNames')
 
-    keys_in_fifths = list(sideman.keys_in_fifths())
-    for i in range(0, len(keys_in_fifths), 2):
-        jazz_scale1 = sideman.JazzScale(keys_in_fifths[i])
-        jazz_scale2 = sideman.JazzScale(keys_in_fifths[i+1])
-        pattern_measure = Measure((4, 4))
-        chord_measure = Measure((4, 4))
-        get_pattern8(jazz_scale1, pattern_measure)
-        get_pattern8(jazz_scale2, pattern_measure)
+    for key in sideman.keys_in_fifths():
+        jazz_scale = sideman.JazzScale(key)
+        pattern_measure = get_pattern8(jazz_scale)
+        chord_measure =get_pattern8_chord_measure(jazz_scale)
         treble_pattern.append( pattern_measure )
-        get_pattern8_chord_measure(jazz_scale1, chord_measure)
-        get_pattern8_chord_measure(jazz_scale2, chord_measure)
         chords.append( chord_measure )
 
-    tempo = Tempo(Duration(1, 4), (100, 138))
+    # Merge all the pairs of 2/4 measures into 4/4 measures
+
+    # TODO: Can probably just put a loop over both staves...
+    
+    staves = [chords, treble_pattern]
+    for staff in staves:
+        parts = sequencetools.partition_sequence_by_counts(staff[:], [2], cyclic=True)
+        for part in parts:
+            mutate(part).fuse()
+
+    tempo = Tempo(Duration(1, 4), (100,138))
     attach(tempo, treble_pattern)
     score = Score([chords, treble_pattern])
     return score
@@ -54,10 +60,10 @@ def composer():
     return "Jerry Greene et al, Thiruvathukal"
 
 def pdf():
-    return "jazz8.pdf"
+    return "jazz8fuse.pdf"
 
 def midi():
-    return "jazz8.midi"
+    return "jazz8fuse.midi"
 
 if __name__ == '__main__':
     common.main( get_score(), title(), composer(), pdf())
