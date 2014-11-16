c2736ba95d69190968f50d7f13bc1269c13f6667
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-11-16 12:31:04 -0600

    Just use pip to install Abjad (for now)
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- scripts/travis/install-abjad.sh
+++ scripts/travis/install-abjad.sh
@@ -1,10 +1,1 @@
-#
-# We install Abjad from repository as opposed to pip to ensure we have the latest developer
-# code. This usually doesn't break anything!
-#
-
-git checkout https://github.com/Abjad/abjad.git
-cd abjad
-python setup.py build
-python setup.py install
-cd ..
+pip install abjad
