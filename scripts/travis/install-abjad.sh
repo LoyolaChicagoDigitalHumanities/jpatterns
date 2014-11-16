04783bfc03404e98a26c6d90522c47e17bc4d7af
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-11-16 12:17:14 -0600

    Add comment about the Abjad build script
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- scripts/travis/install-abjad.sh
+++ scripts/travis/install-abjad.sh
@@ -1,3 +1,8 @@
+#
+# We install Abjad from repository as opposed to pip to ensure we have the latest developer
+# code. This usually doesn't break anything!
+#
+
 git checkout https://github.com/Abjad/abjad.git
 pushd abjad
 python setup.py build
