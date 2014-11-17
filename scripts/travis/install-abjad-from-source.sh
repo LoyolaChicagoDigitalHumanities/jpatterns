760e945fd7fa9fa499dd1658998e88c8db531fcf
George K. Thiruvathukal <gkt@cs.luc.edu>
2014-11-16 21:04:01 -0600

    Updates to Travis CI build
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- scripts/travis/install-abjad-from-source.sh
+++ scripts/travis/install-abjad-from-source.sh
@@ -1,9 +1,5 @@
-pwd
-cd ..
-pwd
-git clone git://github.com/Abjad/abjad.git 
-cd abjad
+git clone git://github.com/Abjad/abjad.git ../abjad
+cd ../abjad
 python setup.py build
 python setup.py install
 cd ../jpatterns
-pwd
